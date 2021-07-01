07ea48032d53ffbef1768e845313e9edeaed20b1
Jacky <337029381@qq.com>
2021-07-01 16:25:01 +0800

    second commit

--- src/main/java/com/jacky030/git/GItTest.java
+++ src/main/java/com/jacky030/git/GItTest.java
@@ -3,5 +3,6 @@ package com.jacky030.git;
 public class GItTest {
     public static void main(String[] args){
         System.out.println("Hello Git");
+        System.out.println("Hello Git1");
     }
 }
