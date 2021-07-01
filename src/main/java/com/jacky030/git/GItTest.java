2e422b2fa10b8ea61de86c5bb4e1f0cfa69e92c9
Jacky <337029381@qq.com>
2021-07-01 16:50:26 +0800

    third commit

--- src/main/java/com/jacky030/git/GItTest.java
+++ src/main/java/com/jacky030/git/GItTest.java
@@ -5,5 +5,6 @@ public class GItTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git1");
         System.out.println("Hello Git111");
+        System.out.println("Hello Git hot-fix");
     }
 }
